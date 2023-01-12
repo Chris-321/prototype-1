0fc258880521995cecc953dd12e1fcb84bec7aea
Chris-321 <chcortesbanda@s.sfusd.edu>
2023-01-12 12:29:29 -0800

    initial commit

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    private float speed = 20.0f;
+    private float turnspeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+
+   
+
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on verticla input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Moves the car forward based on  input
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+    }
+}
